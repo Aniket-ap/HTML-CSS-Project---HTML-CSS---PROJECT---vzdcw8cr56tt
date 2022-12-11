ab2609c2575a89c24c77474bdaa94ce7d25e5f2b
Aniket Pradhan <aniketpradhan814@gmail.com>
2022-12-11 10:31:32 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
 
+let imgUrlArr = [
+  "https://images.unsplash.com/photo-1618220179428-22790b461013?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80",
+  "https://images.unsplash.com/photo-1518780664697-55e3ad937233?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
+  "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+];
+
+var navContainer = document.getElementById("nav-menu");
+
+var navs = navContainer.getElementsByClassName("list");
+
+for (var i = 0; i < navs.length; i++) {
+  navs[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+    var ranDomIndex = Math.floor(Math.random() * 3);
+    document.getElementById("imageId").src = "";
+    document.getElementById("imageId").src = imgUrlArr[ranDomIndex];
+  });
+}
